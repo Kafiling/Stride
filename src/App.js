9b5ef4b4984e29ed0ae2da778054a28738433bd0
Kafiling <kafilkafilth@gmail.com>
2022-03-31 13:33:26 +0700

    Add Scheduler

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAByVAgqZdl4VGXO4l1aA5svaOHh_OCOaA",
-  authDomain: "my-stride.firebaseapp.com",
-  projectId: "my-stride",
-  storageBucket: "my-stride.appspot.com",
-  messagingSenderId: "902869156644",
-  appId: "1:902869156644:web:601c5dd5cf9bcc5be629b1",
-  measurementId: "G-NW7GVF488Q"
-};
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+import SchedulerComponent from './components/Scheduler.js';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+//ยังไม่ Login
+  if(false){ 
+    return (
+    <>
+      <div className="App">
+      <h1>hello</h1>
+      </div>
+    </>
+    );
+}
+//Login แล้ว
+  else{
+    return (
+      <>
+        <div className="App">
+        <SchedulerComponent/>
+        </div>
+      </>
+      );
+  }
+ 
 }
 
 export default App;
